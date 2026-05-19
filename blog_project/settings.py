cce017edf5add989c2c458a20235bf53e4e6a857
Vlad Panov <vladpanov@MacBook-Air-Vlad.local>
2026-05-19 23:05:07 +0500

    Switch database to PostgreSQL using Docker Compose

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -54,8 +54,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'blogdb',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
