566597d28bfbf1ae50fcc1981f685a99ec4f1ddd
Vlad Panov <vladpanov@MacBook-Air-Vlad.local>
2026-05-19 22:33:36 +0500

    Add custom template tag for debugging and clean home.html

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -43,6 +43,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'debug': 'blog.templatetags.debug',
+            },
         },
     },
 ]
